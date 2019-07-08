4f23aff147f16a5ac54676a5aa175d443d26f9c8
tommygod3 <tommygod1@live.com>
2019-07-08 11:23:07 +0000

    Removed range test

--- tests/test_wps_nameparams.py
+++ tests/test_wps_nameparams.py
@@ -3,12 +3,12 @@ import pytest
 from pywps import Service
 from pywps.tests import assert_response_success
 
-from tests.common import client_for
+from tests.common import client_for, get_output
 from swallow.processes.wps_nameparams import RunNAME
 
 
 @pytest.mark.online
-def test_wps_nameparams():
+def test_wps_nameparams_success():
     client = client_for(Service(processes=[RunNAME()]))
     datainputs = "title={};runBackwards={};time={};elevationOut={};resolution={};startdate={};enddate={};" \
                  "domain={};timestamp={}".format(
@@ -22,13 +22,32 @@ def test_wps_nameparams():
         "-180,80,-90,90",
         "3-hourly"
     )
-    print(datainputs)
     resp = client.get(
         service='wps', request='execute', version='1.0.0',
         identifier='runname',
         datainputs=datainputs)
-    print("data")
-    print(resp.data)
-    print("xml")
-    print(resp.xml)
     assert_response_success(resp)
+
+@pytest.mark.skip
+def test_wps_nameparams_filename():
+    client = client_for(Service(processes=[RunNAME()]))
+    datainputs = "title={};runBackwards={};time={};elevationOut={};resolution={};startdate={};enddate={};" \
+                 "domain={};timestamp={}".format(
+        "CAPEVERDE",
+        "true",
+        "1",
+        "0-100",
+        "0.25",
+        "2017-11-01",
+        "2017-11-02",
+        "-180,80,-90,90",
+        "3-hourly"
+    )
+    resp = client.get(
+        service='wps', request='execute', version='1.0.0',
+        identifier='runname',
+        datainputs=datainputs)
+    print(get_output(resp.xml))
+    assert_response_success(resp)#WIP
+
+#{'runid': 'BCK1_3-hourly_CAPEVERDE_1562579076', 'FileContents': 'file:///tmp/ff31948a-a164-11e9-869f-05df0156c2ef/BCK1_3-hourly_CAPEVERDE_1562579076.zip', 'SummaryPlot': 'file:///tmp/ff31948a-a164-11e9-869f-05df0156c2ef/CAPEVERDE_0-100m_summed_all.png'}
