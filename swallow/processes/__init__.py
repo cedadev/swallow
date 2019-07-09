f621da7325fbd3560744a66614e3e77b4950a94b
tommygod3 <tommygod1@live.com>
2019-07-09 14:14:43 +0000

    Another fix to class name change RunNAMEPreset

--- swallow/processes/__init__.py
+++ swallow/processes/__init__.py
@@ -1,9 +1,9 @@
 from .wps_run_name import RunNAME
-from .wps_run_name_preset import RunNamePreset
+from .wps_run_name_preset import RunNAMEPreset
 from .wps_plot_name import PlotNAME
 
 processes = [
     RunNAME(),
-    RunNamePreset(),
+    RunNAMEPreset(),
     PlotNAME(),
 ]
