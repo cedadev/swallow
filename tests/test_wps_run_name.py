ed2e9691283505747d5e2938e8cb426a57e2d247
tommygod3 <tommygod1@live.com>
2019-07-23 08:07:57 +0000

    Reworked run_name and run_name_preset tests

--- tests/test_wps_run_name.py
+++ tests/test_wps_run_name.py
@@ -1,53 +1,48 @@
+import glob
 import pytest
 
 from pywps import Service
 from pywps.tests import assert_response_success
 
-from tests.common import client_for, get_output
+from tests.common import client_for, custom_test_inputs, valid_output_file
 from swallow.processes.wps_run_name import RunNAME
 
 
-@pytest.mark.online
-def test_wps_run_name_success():
+@pytest.mark.skip
+@pytest.mark.parametrize('inputs', custom_test_inputs)
+def test_wps_run_name_inputs_success(inputs):
     client = client_for(Service(processes=[RunNAME()]))
-    datainputs = 'title={};runBackwards={};time={};elevationOut={};resolution={};startdate={};enddate={};' \
-                 'domain={};timestamp={}'.format(
-        'CAPEVERDE',
-        'true',
-        '1',
-        '0-100',
-        '0.25',
-        '2017-11-01',
-        '2017-11-02',
-        '-180,80,-90,90',
-        '3-hourly'
-    )
+    datainputs = (
+        'title={title};'
+        'longitude={longitude};'
+        'latitude={latitude};'
+        'runBackwards={runBackwards};'
+        'time={time};'
+        'elevationOut={elevationOut};'
+        'resolution={resolution};'
+        'startdate={startdate};'
+        'enddate={enddate};'
+        'min_lon={min_lon};'
+        'max_lon={max_lon};'
+        'min_lat={min_lat};'
+        'max_lat={max_lat};'
+        'timestamp={timestamp}'
+    ).format(**inputs)
+
     resp = client.get(
         service='wps', request='execute', version='1.0.0',
-        identifier='runname',
+        identifier='run_name',
         datainputs=datainputs)
     assert_response_success(resp)
 
-@pytest.mark.skip
-def test_wps_run_name_filename():
-    client = client_for(Service(processes=[RunNAME()]))
-    datainputs = 'title={};runBackwards={};time={};elevationOut={};resolution={};startdate={};enddate={};' \
-                 'domain={};timestamp={}'.format(
-        'CAPEVERDE',
-        'true',
-        '1',
-        '0-100',
-        '0.25',
-        '2017-11-01',
-        '2017-11-02',
-        '-180,80,-90,90',
-        '3-hourly'
-    )
-    resp = client.get(
-        service='wps', request='execute', version='1.0.0',
-        identifier='runname',
-        datainputs=datainputs)
-    print(get_output(resp.xml))
-    assert_response_success(resp)#WIP
-
-#{'runid': 'BCK1_3-hourly_CAPEVERDE_1562579076', 'FileContents': 'file:///tmp/ff31948a-a164-11e9-869f-05df0156c2ef/BCK1_3-hourly_CAPEVERDE_1562579076.zip', 'SummaryPlot': 'file:///tmp/ff31948a-a164-11e9-869f-05df0156c2ef/CAPEVERDE_0-100m_summed_all.png'}
+@pytest.mark.online
+def test_wps_run_name_tokyo_outputs(custom_tokyo_outputs):
+    output_files = glob.glob(f'{custom_tokyo_outputs}/outputs/*.txt')
+    for output in output_files:
+        assert valid_output_file(output, 'custom_tokyo')
+        
+@pytest.mark.online
+def test_wps_run_name_egypt_outputs(custom_egypt_outputs):
+    output_files = glob.glob(f'{custom_egypt_outputs}/outputs/*.txt')
+    for output in output_files:
+        assert valid_output_file(output, 'custom_egypt')
