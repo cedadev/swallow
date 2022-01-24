bda2928d0282585848c784aad4827ec2a5a2ff2d
Alan Iwi <alan.iwi@stfc.ac.uk>
2022-01-24 16:19:26 +0000

    add initial mocked up RunNAMETrajectory process and tests

---
+++ swallow/processes/wps_run_name_trajectory.py
@@ -0,0 +1,192 @@
+from pywps import (Process, LiteralInput, LiteralOutput,
+                   BoundingBoxInput, BoundingBoxOutput, UOM)
+from pywps.app.Common import Metadata
+#from pywps.validator.mode import MODE
+
+import datetime
+import logging
+LOGGER = logging.getLogger("PYWPS")
+
+
+class RunNAMETrajectory(Process):
+    """Run the NAME trajectory model."""
+    def __init__(self):
+
+        #-----------------------------------------
+        # Note: docs at
+        # https://pywps.readthedocs.io/en/latest/api.html
+        #
+        # and to see allowed values of data_type:
+        #   from pywps.inout.literaltypes import LITERAL_DATA_TYPES
+        #   print(LITERAL_DATA_TYPES)
+        #-----------------------------------------
+
+        inputs = [
+            LiteralInput('RunID', 'run identifier',
+                         abstract='short text string to describe task',
+                         data_type='string',
+                         min_occurs=1,
+                         max_occurs=1),
+
+            LiteralInput('Description', 'description',
+                         abstract='longer text description',
+                         data_type='string',
+                         min_occurs=0,
+                         max_occurs=1),
+
+            LiteralInput('KnownLocation',
+                         abstract='known location',
+                         data_type='string',
+                         min_occurs=0,
+                         max_occurs=1),
+            #(optional): from a list of labelled location
+
+            LiteralInput('Latitude',
+                         abstract='latitude of trajectory start/end-point',
+                         data_type='float',
+                         min_occurs=0,
+                         max_occurs=1),
+            
+            LiteralInput('Longitude',
+                         abstract='longitude of trajectory start/end-point',
+                         data_type='float',
+                         min_occurs=0,
+                         max_occurs=1),
+
+            LiteralInput('ReleaseTime',
+                         abstract='date and time when particles are released',
+                         data_type='dateTime',
+                         min_occurs=0,
+                         max_occurs=1),
+            
+            LiteralInput('RunDuration',
+                         abstract='duration of trajectory run, in hours',
+                         data_type='integer',
+                         allowed_values=[12, 24, 36, 48, 72, 96, 120, 144, 168, 240, 360, 480],
+                         min_occurs=1,
+                         max_occurs=1),
+
+            LiteralInput('RunDirection',
+                         abstract='whether to run forward or backward trajectories',
+                         data_type='string',
+                         allowed_values=['Forward', 'Backward'],
+                         min_occurs=1,
+                         max_occurs=1),
+
+            LiteralInput('TrajectoryHeights',
+                         abstract='array of start/end heights of particles',
+                         data_type='float',
+                         min_occurs=1,
+                         ),
+
+            LiteralInput('TrajectoryHeightUnits',
+                         abstract='units for the TrajectoryHeights array',
+                         data_type='string',
+                         allowed_values=['metres above ground level (m agl)', 'metres above sea level (m asl)'],
+                         min_occurs=1,
+                         max_occurs=1),
+
+            LiteralInput('MetData',
+                         abstract='which forcing dataset to use to drive the trajectory model',
+                         data_type='string',
+                         allowed_values=["Global", "UK 1.5km", "Global + UK 1.5km"],
+                         min_occurs=1,
+                         max_occurs=1),
+
+            LiteralInput('NotificationEmail',
+                         abstract='which email address to send notifications to',
+                         data_type='string',
+                         min_occurs=0,
+                         max_occurs=1),
+
+            LiteralInput('ImageFormat',
+                         abstract='format of output image file',
+                         data_type='string',
+                         default='PNG',
+                         allowed_values=['PNG', 'JPG', 'PDF'],
+                         min_occurs=1,
+                         max_occurs=1),
+            
+        ]
+        outputs = [
+            LiteralOutput('echo', 'Example "echo" output',
+                          abstract='This output just gives a string confirming the inputs used.',
+                          keywords=['output', 'result', 'response'],
+                          data_type='string'),
+        ]
+
+        super(RunNAMETrajectory, self).__init__(
+            self._handler,
+            identifier='run_trajectory',
+            title='Run NAME Trajectory',
+            abstract='A forwards or backwards run of the NAME model outputting particle trajectories following the mean flow only.',
+            keywords=["name", "model", "atmospheric", "dispersion", "trajectory", "forward", "backward", "air", "pollution", "transport"],
+            metadata=[
+                Metadata('PyWPS', 'https://pywps.org/'),
+                Metadata('Birdhouse', 'http://bird-house.github.io/'),
+                Metadata('PyWPS Demo', 'https://pywps-demo.readthedocs.io/en/latest/'),
+                Metadata('Emu: PyWPS examples', 'https://emu.readthedocs.io/en/latest/'),
+            ],
+            version='1.5',
+            inputs=inputs,
+            outputs=outputs,
+            store_supported=True,
+            status_supported=True
+        )
+
+    @staticmethod
+    def _get_input(request, key, multi=False):
+
+        inputs = request.inputs.get(key)
+
+        if inputs == None:
+            return None
+        
+        if multi:
+            return [inp.data for inp in inputs]
+        else:
+            inp, = inputs
+            return inp.data
+        
+
+    @staticmethod
+    def _handler(request, response):
+        LOGGER.info("run NAME trajectory")
+
+        # need default value of ReleaseTime (   # default to 00Z on 1st Jan of current year)
+
+        runID = RunNAMETrajectory._get_input(request, 'RunID')
+        description = RunNAMETrajectory._get_input(request, 'Description')
+        known_location = RunNAMETrajectory._get_input(request, 'KnownLocation')
+        latitude = RunNAMETrajectory._get_input(request, 'Latitude')
+        longitude = RunNAMETrajectory._get_input(request, 'Longitude')
+        release_time = RunNAMETrajectory._get_input(request, 'ReleaseTime')
+        run_duration = RunNAMETrajectory._get_input(request, 'RunDuration')
+        run_direction = RunNAMETrajectory._get_input(request, 'RunDirection')
+        trajectory_heights = RunNAMETrajectory._get_input(request, 'TrajectoryHeights', multi=True)
+        trajectory_height_units = RunNAMETrajectory._get_input(request, 'TrajectoryHeightUnits')
+        met_data = RunNAMETrajectory._get_input(request, 'MetData')
+        notification_email = RunNAMETrajectory._get_input(request, 'NotificationEmail')
+        image_format = RunNAMETrajectory._get_input(request, 'ImageFormat')
+
+        if release_time == None:
+            year = datetime.datetime.now().year
+            release_time = datetime.datetime(year, 1, 1)
+        
+        response.outputs['echo'].data = (
+            f'runID: {runID}, '
+            f'description: {description}, '
+            f'known_location: {known_location}, '
+            f'latitude: {latitude}, '
+            f'longitude: {longitude}, '
+            f'release_time: {release_time}, '
+            f'run_duration: {run_duration}, '
+            f'run_direction: {run_direction}, '
+            f'trajectory_heights: {trajectory_heights}, '
+            f'trajectory_height_units: {trajectory_height_units}, '
+            f'met_data: {met_data}, '
+            f'notification_email: {notification_email}, '
+            f'image_format: {image_format}'
+            )
+
+        return response
--- tests/test_swallow.py
+++ tests/test_swallow.py
@@ -2,25 +2,76 @@
 
 """Tests for `swallow` package."""
 
-import pytest
+from pywps import Service
+from pywps.tests import client_for, assert_response_success
 
-from click.testing import CliRunner
+import urllib.parse
+import re
 
-import swallow
-from swallow import cli
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+from .common import get_output, show_tree
+from swallow.processes.wps_run_name_trajectory import RunNAMETrajectory
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+
+def _get_test_inputs():
+    return dict([('RunID', 'myrun'),
+                 ('RunDuration', '24'),
+                 ('RunDirection', 'Forward'),
+                 ('TrajectoryHeights', '10.3'),
+                 ('TrajectoryHeightUnits', 'metres above ground level (m agl)'),
+                 ('MetData', 'Global'),
+                 ('ImageFormat', 'PNG')])
+
+
+def _get_inputs_string(inputs):
+    return ';'.join(f'{k}={urllib.parse.quote_plus(v)}' for k, v in inputs.items())
+
+def _assert_response_failure(resp):
+    assert resp.status_code % 100 != 2
+
+def _get_response(inputs):
+    client = client_for(Service(processes=[RunNAMETrajectory()]))
+
+    resp = client.get(
+        "?service=WPS&request=Execute&version=1.0.0&identifier=run_trajectory"
+        f"&datainputs={_get_inputs_string(inputs)}")
+
+    return resp
+    
+
+def test_wps_run_name_trajectory():
+
+    inputs = _get_test_inputs()
+    resp = _get_response(inputs)
+    assert_response_success(resp)
+    #====================
+    # dump some things to stdout:
+    #   to see this run pytest with the "-s" option to disable output capture
+    #   e.g.   python -m pytest -W ignore -s tests
+    show_tree(resp.xml)
+    #====================
+    outputs = get_output(resp.xml)
+    print(outputs)
+
+    echo_outputs = outputs['echo']
+    ref_outputs = 'runID: myrun, description: None, known_location: None, latitude: None, longitude: None, release_time: yyyy-01-01 00:00:00, run_duration: 24, run_direction: Forward, trajectory_heights: [10.3], trajectory_height_units: metres above ground level (m agl), met_data: Global, notification_email: None, image_format: PNG'
+
+    print(echo_outputs)
+    print(ref_outputs)
+    
+    assert re.sub('20\d{2}-01-01', 'yyyy-01-01', echo_outputs) == ref_outputs
+
+    
+def test_wps_run_name_trajectory_missing_compulsory_input():
+    inputs = _get_test_inputs()
+    del inputs['RunDuration']
+    resp = _get_response(inputs)
+    _assert_response_failure(resp)
+    
+
+def test_wps_run_name_trajectory_with_disallowed_value():
+    inputs = _get_test_inputs()
+    inputs['RunDuration'] = '25'
+    resp = _get_response(inputs)
+    _assert_response_failure(resp)
+    
