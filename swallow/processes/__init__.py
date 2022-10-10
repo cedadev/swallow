edc6f79299740fb630073c92df8419e31cfaf04a
Ag Stephens <ag.stephens@stfc.ac.uk>
2022-10-10 04:28:19 +0100

    add air history on list of run types

--- swallow/processes/__init__.py
+++ swallow/processes/__init__.py
@@ -1,9 +1,11 @@
 from .wps_run_name_trajectory import RunNAMETrajectory
 from .wps_extract_met_data import ExtractMetData
 from .wps_general_forward_run import GenForwardRun
+from .wps_air_history_run import AirHistoryRun
 
 processes = [
     RunNAMETrajectory(),
     ExtractMetData(),
     GenForwardRun(),
+    AirHistoryRun(),
 ]
